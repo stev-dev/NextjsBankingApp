d537e2c3d80e7c9004e0acc54ca10c5069163222
Mostapha <hah.f16@gmail.com>
2024-08-15 08:27:02 +0100

    initial setup and routing setup

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,13 @@
+import React, { Children } from "react";
+
+export default function RootLayout(
+    {children}: Readonly<{
+        children: React.ReactNode;}>) 
+    {
+    return (
+    <main>
+        SIDEBAR
+        {children}
+    </main>
+    );
+}
