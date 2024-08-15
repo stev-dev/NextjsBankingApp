b888bcb79e2cf38c428164f22cd3d45436116c04
Mostapha <hah.f16@gmail.com>
2024-08-15 18:17:31 +0100

    doing this commit from vscode maybe it's duplicate commit idk

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,5 @@
-import HeaderBox from "@/app/components/ui/HeaderBox"
-import TotalBalanceBox from "../components/ui/TotalBalanceBox"
+import HeaderBox from "@/components/HeaderBox"
+import TotalBalanceBox from "@/components/TotalBalanceBox"
 
 const Home = ({accounts}:TotlaBalanceBoxProps) => {
   const loggedIn ={firstname: "Mostapha"}
